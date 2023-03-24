648e8ae7d371640a93841fdef00104cf052007a2
uchida-yu <yu3shabadaba+github@gmail.com>
2023-03-24 10:49:23 +0900

    初期設定

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import '@/styles/App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <a href="/practice1.html">practice1</a>
+    </div>
+  );
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sassDts from 'vite-plugin-sass-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles" as common;`,
+        importer(...args) {
+          if (args[0] !== '@/styles') {
+            return undefined;
+          }
+          return {
+            file: `${path.resolve(__dirname, './src/styles')}`,
+          };
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    sassDts({
+      enabledMode: ['development', 'production'],
+      global: {
+        generate: true,
+        outFile: path.resolve(__dirname, './src/styles/style.d.ts'),
+      },
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
+});
